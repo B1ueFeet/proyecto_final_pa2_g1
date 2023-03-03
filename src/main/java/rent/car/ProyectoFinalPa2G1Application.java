c219ac17f93c9973cb4fc5665ab1e291d83ab7c0
BlueFeet <b1feet.dev@uce.edu.ec>
2023-03-03 18:34:34 -0500

    Primer Commit

---
+++ src/main/java/rent/car/ProyectoFinalPa2G1Application.java
@@ -0,0 +1,13 @@
+package rent.car;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoFinalPa2G1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoFinalPa2G1Application.class, args);
+	}
+
+}
---
+++ src/test/java/rent/car/ProyectoFinalPa2G1ApplicationTests.java
@@ -0,0 +1,13 @@
+package rent.car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoFinalPa2G1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
