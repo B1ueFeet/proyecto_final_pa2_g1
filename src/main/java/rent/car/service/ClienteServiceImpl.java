578e7140cbb5c89a7db50a745f8803ced3f22179
Henry17C <hicoyago@uce.edu.ec>
2023-03-04 16:05:06 -0500

    Cliente Service

--- src/main/java/rent/car/service/ClienteServiceImpl.java
+++ src/main/java/rent/car/service/ClienteServiceImpl.java
@@ -1,5 +1,37 @@
 package rent.car.service;
 
-public class ClienteServiceImpl {
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import rent.car.modelo.Cliente;
+import rent.car.repository.IClienteRepository;
+import rent.car.repository.IEmpleadoRepository;
+@Service
+public class ClienteServiceImpl implements IClienteService {
+
+	@Autowired
+	private IClienteRepository clienteRepository;
+	
+	
+	 @Override
+		public void registrarComoCliente(String registro) {
+			// TODO Auto-generated method stub
+			
+			Cliente cliente = new Cliente();
+			cliente.setRegistro(registro);
+					
+			if (cliente.getRegistro().equals("C")) {
+			clienteRepository.registrarseCliente(cliente);
+			
+			}
+			
+			
+			
+		}
+
+
+
+	
+	
 }
