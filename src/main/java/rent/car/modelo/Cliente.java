a45b74b308863f4bffd69a10a3df28506f8302a3
Henry17C <hicoyago@uce.edu.ec>
2023-03-03 20:04:32 -0500

    Entidad usuario

---
+++ src/main/java/rent/car/modelo/Cliente.java
@@ -0,0 +1,136 @@
+package rent.car.modelo;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.SequenceGenerator;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="cliente")
+public class Cliente {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "seq_clie")
+	@SequenceGenerator(name = "seq_clie", sequenceName = "seq_clie", allocationSize = 1)
+	@Column(name = "clie_id")
+	private Integer id;
+	
+	@Column(name = "clie_cedula")
+	private String cedula;
+	
+	@Column(name = "clie_nombre")
+	private String  nombre;
+	
+	@Column(name = "clie_apellido")
+	private String  apellido;
+	
+	@Column(name = "clie_usuario")
+	private String  usuario;
+	
+	@Column(name = "clie_contrasenia")
+	private String  contrasenia;
+	
+	@Column(name = "clie_fechaNacimiento")
+	private LocalDateTime fechaNacimiento;
+	
+	@Column(name = "clie_genero")
+	private String genero;
+	
+	@Column(name = "clie_registro")
+	private String registro;
+	
+	
+	
+	//get y set
+
+	@Override
+	public String toString() {
+		return "Cliente [id=" + id + ", cedula=" + cedula + ", nombre=" + nombre + ", apellido=" + apellido
+				+ ", usuario=" + usuario + ", contrasenia=" + contrasenia + ", fechaNacimiento=" + fechaNacimiento
+				+ ", genero=" + genero + ", registro=" + registro + "]";
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+
+	public String getContrasenia() {
+		return contrasenia;
+	}
+
+	public void setContrasenia(String contrasenia) {
+		this.contrasenia = contrasenia;
+	}
+
+	public LocalDateTime getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(LocalDateTime fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getRegistro() {
+		return registro;
+	}
+
+	public void setRegistro(String registro) {
+		this.registro = registro;
+	}
+	
+	
+	 
+	
+	
+	
+
+}
