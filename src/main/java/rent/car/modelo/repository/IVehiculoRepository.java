8ea0ca4fcca541c604514b6cdb1e67a92d951a2e
Willan Diaz <wadiaz@uce.edu.ec>
2023-03-03 21:29:01 -0500

    Repository de vehiculo

---
+++ src/main/java/rent/car/modelo/repository/IVehiculoRepository.java
@@ -0,0 +1,18 @@
+package rent.car.modelo.repository;
+
+import java.util.List;
+
+import rent.car.modelo.Vehiculo;
+
+public interface IVehiculoRepository {
+
+	public void insertar(Vehiculo vehiculo);
+
+	public List<Vehiculo> buscarPorMarca(String marca);
+
+	public Vehiculo buscar(Integer id);
+
+	public void actualizar(Vehiculo vehiculo);
+
+	public void eliminar(Integer id);
+}
