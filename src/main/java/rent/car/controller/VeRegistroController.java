11e8d7d8f54325a99e3d3a197b955997cdff98a1
BlueFeet <b1feet.dev@uce.edu.ec>
2023-03-07 17:47:03 -0500

    unido casi completo

--- src/main/java/rent/car/controller/VeRegistroController.java
+++ src/main/java/rent/car/controller/VeRegistroController.java
@@ -10,11 +10,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import rent.car.modelo.Cliente;
 import rent.car.modelo.Reserva;
 import rent.car.modelo.Vehiculo;
+import rent.car.service.IClienteService;
 import rent.car.service.IReservaService;
 import rent.car.service.IVehiculoService;
-
 
 @Controller
 @RequestMapping("/clientes")
@@ -22,18 +23,20 @@ public class VeRegistroController {
 
 	@Autowired
 	private IVehiculoService iVehiculoService;
-	
+
 	@Autowired
 	private IReservaService iReservaService;
-	
-	
+
+	@Autowired
+	private IClienteService iClienteService;
+
 	@GetMapping("/reservar")
 	public String paginaBuscarTodosVehiculos(Model model) {
 		List<Vehiculo> listaVehiculos = this.iVehiculoService.buscarTodos();
 		model.addAttribute("vehiculos", listaVehiculos);
 		return "vistaInicioReserva";
 	}
-	
+
 	@GetMapping("/buscarplaca/{placa}")
 	public String buscarPorPlaca(@PathVariable("placa") String placa, Model model, Reserva reserva) {
 		Vehiculo vehiculo = this.iVehiculoService.buscarPlaca(placa);
@@ -41,13 +44,21 @@ public class VeRegistroController {
 		model.addAttribute("reserva", reserva);
 		return "vistaSecundariaReserva2";
 	}
-	
-	
+
 	@PostMapping("/insertar")
 	public String insertarReserva(Reserva reserva) {
 		this.iReservaService.agregar(reserva);
 		return "vistaReserva";
 	}
-	
 
+	@GetMapping("/registro")
+	public String paginaInicio(Cliente cliente) {
+		return "vGuardarCC";
+	}
+
+	@PostMapping("/insertar/cliente")
+	public String insertarCliente(Cliente cliente) {
+		this.iClienteService.registrar("C", cliente);
+		return "redirect: /clientes/inicio";
+	}
 }
