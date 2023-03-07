bd94fdd1dd94f5719899c6ef851b5b069628d55a
BlueFeet <b1feet.dev@uce.edu.ec>
2023-03-07 17:57:59 -0500

    william final

--- src/main/java/rent/car/controller/EmpleadoController.java
+++ src/main/java/rent/car/controller/EmpleadoController.java
@@ -6,25 +6,18 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.DeleteMapping;
-=======
->>>>>>> refs/remotes/origin/willan
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import jakarta.websocket.server.PathParam;
 import rent.car.modelo.Cliente;
 import rent.car.modelo.Vehiculo;
 import rent.car.service.IClienteService;
-<<<<<<< HEAD
-import rent.car.service.IVehiculoService;
-=======
 import rent.car.service.IReservaService;
->>>>>>> refs/remotes/origin/willan
+import rent.car.service.IVehiculoService;
 
 @Controller
 @RequestMapping("/empleados")
@@ -35,7 +28,7 @@ public class EmpleadoController {
 
 	@Autowired
 	private IClienteService clienteService;
-	
+
 	@Autowired
 	private IVehiculoService vehiculoService;
 
@@ -58,70 +51,67 @@ public class EmpleadoController {
 		this.clienteService.registrar("E", cliente);
 		return "guardado";
 	}
-	
-	
+
 	@GetMapping("/buscar")
 	public String buscarClientes(Model model) {
-		List< Cliente> lista = this.clienteService.encontrarTodos();
-		model.addAttribute("empleados",lista);
+		List<Cliente> lista = this.clienteService.encontrarTodos();
+		model.addAttribute("empleados", lista);
 		return "vistaListaEmpleadoCliente";
 	}
-    @DeleteMapping("/borrar/{id}")
-    public String borrarCliente(@PathVariable("id") Integer id) {
-    	this.clienteService.borrar(id);
-    	return "redirect:/empleados/buscar";
-    	
-    }
-    
-    @GetMapping("/buscarPorApellido/{apellido}")
-    
-    public String buscarPorApellido(@PathVariable("apellido")String apellido, Model model) {
-    	ArrayList<Cliente> cliente =(ArrayList<Cliente>) this.clienteService.buscarApellido(apellido);
-    	model.addAttribute("apellido", cliente);
-    	return "vistaActualizarEmpleadoCliente";
-    }
-    @PutMapping("actualizar/{apellido}")
-    public String buscarPorApellido(@PathVariable("apellido")String apellido,Cliente cliente) {
-    	cliente.setApellido(apellido);
-    	this.clienteService.buscarApellido(apellido);
-    	return "redirect:/empleados/buscar";
-    	
-    	
-    }
-	//Vehiculo
-	
+
+	@DeleteMapping("/borrar/{id}")
+	public String borrarCliente(@PathVariable("id") Integer id) {
+		this.clienteService.borrar(id);
+		return "redirect:/empleados/buscar";
+
+	}
+
+	@GetMapping("/buscarPorApellido/{apellido}")
+
+	public String buscarPorApellido(@PathVariable("apellido") String apellido, Model model) {
+		ArrayList<Cliente> cliente = (ArrayList<Cliente>) this.clienteService.buscarApellido(apellido);
+		model.addAttribute("apellido", cliente);
+		return "vistaActualizarEmpleadoCliente";
+	}
+
+	@PutMapping("actualizar/{apellido}")
+	public String buscarPorApellido(@PathVariable("apellido") String apellido, Cliente cliente) {
+		cliente.setApellido(apellido);
+		this.clienteService.buscarApellido(apellido);
+		return "redirect:/empleados/buscar";
+
+	}
+	// Vehiculo
+
 	@GetMapping("/inicio/registrovehi") // va a tomar referencia a la raiz de nuestra aplicacion
 	public String paginaInicio(Vehiculo vehiculo) {
-
-<<<<<<< HEAD
 		return "vistaRegistroVehiculo";
 	}
-	
+
 	@PostMapping("/insertarV")
 	public String insertarVehiculo(Vehiculo vehiculo) {
 		this.vehiculoService.guardar(vehiculo);
 		return "guardado";
 	}
-	
+
 	@GetMapping("/buscarV")
 	public String buscarVehiculo(Model model) {
 		List<Vehiculo> lista = this.vehiculoService.encontrarTodos();
-		model.addAttribute("empleados",lista);
+		model.addAttribute("empleados", lista);
 		return "vistaListaVehiculoDH";
 	}
-	
+
 	@DeleteMapping("/borrarV/{id}")
 	public String borrarPersona(@PathVariable("id") Integer id) {
-		 this.vehiculoService.borrar(id);
-		
+		this.vehiculoService.borrar(id);
+
 		return "redirect:/empleados/buscarV";
 	}
-=======
+
 	@GetMapping("/reporteReservas")
 	public String reporteReservas(Model model) {
-		//this.reservaService.reporteReserva(null, null);
+		// this.reservaService.reporteReserva(null, null);
 		return "vistaReporteReserva";
 	}
 
->>>>>>> refs/remotes/origin/willan
 }
