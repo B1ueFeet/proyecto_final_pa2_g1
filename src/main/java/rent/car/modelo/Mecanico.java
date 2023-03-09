4225c3215d9fde447a8a6a03be3399ad45a039f2
Henry17C <hicoyago@uce.edu.ec>
2023-03-08 19:21:06 -0500

    henryDefensa

--- src/main/java/rent/car/modelo/Mecanico.java
+++ src/main/java/rent/car/modelo/Mecanico.java
@@ -28,6 +28,38 @@ public class Mecanico {
 
 	@Column(name = "meca_apellido")
 	private String apellido;
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getCedula() {
+		return cedula;
+	}
+
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getApellido() {
+		return apellido;
+	}
+
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
 	
 	
 	
